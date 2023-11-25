7db246cde9ea5f64f982755b1d79d4ddcdcbe4f8
joy <joytambuwun1810@gmail.com>
2023-11-25 23:34:21 +0800

    comm

--- index.php
+++ index.php
@@ -10,75 +10,35 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
 </head>
 
-<body>
+<body style="height: 3000px">
     <!--HEADER-->
-    <nav class="navbar navbar-expand navbar-dark bg-dark">
-        <div class="container-lg">
-            <a class="navbar-brand" href="#"><i class="bi bi-egg-fried"></i> Waroeng Warem</a>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            Username
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu-end mt-2">
-                            <li><a class="dropdown-item" href="#"><i class="bi bi-person-circle"></i> Profile</a></li>
-                            <li><a class="dropdown-item" href="#"><i class="bi bi-gear"></i> Settings</a></li>
-                            <li><a class="dropdown-item" href="#"><i class="bi bi-box-arrow-left"></i> Logout</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <?php include "header.php"; ?>
     <!--END HEADER-->
     <div class="container-lg">
         <div class="row">
             <!--SIDEBAR-->
-            <div class="col-lg-3">
-                <nav class="navbar navbar-expand-lg bg-body-tertiary rounded border mt-2">
-                    <div class="container-fluid">
-                        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
-                            data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar"
-                            aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar"
-                            aria-labelledby="offcanvasNavbarLabel" style="width:250px">
-                            <div class="offcanvas-header">
-                                <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="offcanvas"
-                                    aria-label="Close"></button>
-                            </div>
-                            <div class="offcanvas-body">
-                                <ul class="navbar-nav nav-pills flex-column justify-content-end flex-grow-1 ">
-                                    <li class="nav-item">
-                                        <a class="nav-link active link-light" aria-current="page" href="#"><i class="bi bi-house"></i> Home</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link link-dark" href="#"><i class="bi bi-cart-plus"></i> Order</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link link-dark" href="#"><i class="bi bi-calendar3-event"></i> Tables</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link link-dark" href="#"><i class="bi bi-bag-fill"></i> Product</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </nav>
-            </div>
+            <?php include "sidebar.php"; ?>
             <!--end sidebar-->
 
             <!--CONTENT-->
-            <div class="col-lg-9 bg-dark mt-2">
-
-            </div>
+                <?php
+                if(isset($_GET['x']) && $_GET['x'] == 'home'){
+                    include "home.php";
+                }elseif(isset($_GET['x']) && $_GET['x'] == 'reservasi'){
+                    include "reservasi.php";
+                }elseif(isset($_GET['x']) && $_GET['x'] == 'meja'){
+                    include "meja.php";
+                }elseif(isset($_GET['x']) && $_GET['x'] == 'user'){
+                    include "user.php";
+                }
+                ?>
             <!--END CONTENT-->
         </div>
+        <!--Footer-->
+        <div class="fixed-bottom text-center bg-dark text-light mb-2">
+            Created by Kelompok 7 RPL
+        </div>
+        <!--END FOOTER-->
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
