1565dcd731cfd6c1da140bc56f7c1f642cd3d419
joy <joytambuwun1810@gmail.com>
2023-11-29 20:37:12 +0800

    com

--- User.php
+++ User.php
@@ -27,11 +27,75 @@ while ($record = mysqli_fetch_array($query)) {
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
-                            ...
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Understood</button>
+                            <form <form class="needs-validation" novalidate action="proses/proses_input_user.php"
+                                method="POST">
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <div class="form-floating mb-3">
+                                            <input type="text" class="form-control" id="floatingInput"
+                                                placeholder="Your Name" name="nama" required>
+                                            <label for="floatingInput">Nama</label>
+                                            <div class="invalid-feedback">
+                                                Masukan Nama.
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <div class="form-floating mb-3">
+                                            <input type="email" class="form-control" id="floatingInput"
+                                                placeholder="name@example.com" name="username" required>
+                                            <label for="floatingInput">Email</label>
+                                            <div class="invalid-feedback">
+                                                Email yang dimasukan tidak valid.
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-lg-4">
+                                        <div class="form-floating mb-3">
+                                            <select class="form-select" aria-label="Default select example"
+                                                name="level" required>
+                                                <option selected hidden value="0">Pilih Level User</option>
+                                                <option value="1">Manager/Admin</option>
+                                                <option value="2">Pelayan</option>
+                                                <option value="3">Pelanggan</option>
+                                            </select>
+                                            <label for="floatingInput">Level User</label>
+                                            <div class="invalid-feedback">
+                                                Pilih level user.
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-8">
+                                        <div class="form-floating mb-3">
+                                            <input type="number" class="form-control" id="floatingInput"
+                                                placeholder="08xxxxx"  name="nohp">
+                                            <label for="floatingInput">NO HP</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <div class="form-floating mb-3">
+                                            <input type="password" class="form-control" id="floatingInput"
+                                                placeholder="Password" disabled value="12345" name="password">
+                                            <label for="floatingPassword">Password</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-floating">
+                                    <textarea class="form-control" id="" style="height:100px"
+                                        name="alamat"></textarea>
+                                    <label for="floatingInput">Alamat</label>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary"
+                                        data-bs-dismiss="modal">Close</button>
+                                    <button type="submit" class="btn btn-primary" name="input_user_validate"
+                                        value="12345">Save Changes</button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
@@ -84,7 +148,9 @@ while ($record = mysqli_fetch_array($query)) {
                             foreach ($result as $row) {
                                 ?>
                                 <tr>
-                                    <th scope="row"><?php echo $no++ ?></th>
+                                    <th scope="row">
+                                        <?php echo $no++ ?>
+                                    </th>
                                     <td>
                                         <?php echo $row['nama'] ?>
                                     </td>
@@ -117,3 +183,25 @@ while ($record = mysqli_fetch_array($query)) {
         </div>
     </div>
 </div>
+
+<script>
+    // Example starter JavaScript for disabling form submissions if there are invalid fields
+    (() => {
+        'use strict'
+
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        const forms = document.querySelectorAll('.needs-validation')
+
+        // Loop over them and prevent submission
+        Array.from(forms).forEach(form => {
+            form.addEventListener('submit', event => {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+    })()
+</script>
